cb46b4d0ff27fe8c4fcc93bf71a7dad312cbf9c1
Manthan Trivedi <manthan.trivedi@bito.ai>
2025-08-24 20:39:13 +0530

    initial project for django practice

---
+++ Django/practice/website/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from datetime import datetime
+
+from meetings.models import Meeting
+
+
+def welcome(request):
+    return render(request,"website/welcome.html",
+                  {"num_meetings":Meeting.objects.count(),
+                   "x":42})
+
+def date(request):
+    return HttpResponse("This page was served at" + str(datetime.now()))
+
+def about(request):
+    return HttpResponse("This is a about page")
